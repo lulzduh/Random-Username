9e94ecdb9a5d02da3f72f9bb796c481b03721a75
lulzduh <125925460+lulzduh@users.noreply.github.com>
2024-05-08 21:39:00 -0400

    Actual Script

---
+++ randomuser.py
@@ -0,0 +1,11 @@
+import random
+
+wordlist_one = ["DATA GOES HERE"]
+wordlist_two = ["DATA GOES HERE"]
+random_number = ["DATA GOES HERE"]
+
+word_one = random.choice(wordlist_one)
+word_two = random.choice(wordlist_two)
+random_number = random.choice(random_number)
+
+print("Random Username Is:", str(word_one)+str(word_two)+str(random_number))
